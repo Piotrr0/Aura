0c2a2cc7deb3d6f39cd21f8d11e983ba13306d78
Piotrr0 <piotrr.nawalany@gmail.com>
2024-11-30 12:36:15 +0100

    Attribute info data asset

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,18 @@
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const auto& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+	
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find Attribute info for %s"), *AttributeTag.ToString());
+	}
+	return FAuraAttributeInfo();
+}
