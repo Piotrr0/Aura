0523f8575041ace62e5779c342c8fc27f5299cbd
Piotrr0 <piotrr.nawalany@gmail.com>
2024-12-29 15:59:46 +0100

    Show ability is spell tree

--- Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
@@ -22,6 +22,9 @@ struct FAuraAbilityInfo
 	UPROPERTY(BlueprintReadOnly)
 	FGameplayTag InputTag = FGameplayTag();
 
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayTag StatusTag = FGameplayTag();
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TObjectPtr<const UTexture2D> Icon = nullptr;
 
