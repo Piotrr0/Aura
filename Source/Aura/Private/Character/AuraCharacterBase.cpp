9b449a7e0ab8dd9a1551ecf0669905cdb86d7cf4
Piotrr0 <piotrr.nawalany@gmail.com>
2025-01-02 18:43:33 +0100

    Beam GameplayCue

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -114,6 +114,11 @@ FOnDeath AAuraCharacterBase::GetOnDeathDelegate()
 	return OnDeath;
 }
 
+USkeletalMeshComponent* AAuraCharacterBase::GetWeapon_Implementation()
+{
+	return Weapon;
+}
+
 void AAuraCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
