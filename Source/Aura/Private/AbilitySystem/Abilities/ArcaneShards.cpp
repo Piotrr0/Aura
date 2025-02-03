7406aa1e2b2ab263c98b8031adbedaaa10928680
Piotrr0 <piotrr.nawalany@gmail.com>
2025-02-03 14:48:01 +0100

    Spell descriptions for electrocute and arcane shards

---
+++ Source/Aura/Private/AbilitySystem/Abilities/ArcaneShards.cpp
@@ -0,0 +1,59 @@
+#include "AbilitySystem/Abilities/ArcaneShards.h"
+
+FString UArcaneShards::GetDescription(int32 Level)
+{
+	const int32 ScaledDamage = Damage.GetValueAtLevel(Level);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	if (Level == 1)
+	{
+		return FString::Printf(TEXT(
+			"<Title>ARCANE SHARDS</>\n\n"
+			"<Small>Level: </><Level>%d</>\n"
+			"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+			"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+
+			"<Default>Summon a shard of arcane energy, "
+			"causing radial arcane damage of  </>"
+			"<Damage>%d</><Default> at the shard origin.</>"),
+			Level,
+			ManaCost,
+			Cooldown,
+			ScaledDamage);
+	}
+	else
+	{
+		return FString::Printf(TEXT(
+			"<Title>ARCANE SHARDS</>\n\n"
+			"<Small>Level: </><Level>%d</>\n"
+			"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+			"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+			"<Default>Summon %d shards of arcane energy, causing radial arcane damage of </>"
+			"<Damage>%d</><Default> at the shard origins.</>"),
+			Level,
+			ManaCost,
+			Cooldown,
+			FMath::Min(Level, MaxNumShards),
+			ScaledDamage);
+	}
+}
+
+FString UArcaneShards::GetNextLevelDescription(int32 Level)
+{
+	const int32 ScaledDamage = Damage.GetValueAtLevel(Level);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+
+	return FString::Printf(TEXT(
+		"<Title>NEXT LEVEL: </>\n\n"
+		"<Small>Level: </><Level>%d</>\n"
+		"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+		"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+		"<Default>Summon %d shards of arcane energy, causing radial arcane damage of </>"
+		"<Damage>%d</><Default> at the shard origins.</>"),
+		Level,
+		ManaCost,
+		Cooldown,
+		FMath::Min(Level, MaxNumShards),
+		ScaledDamage);
+}
