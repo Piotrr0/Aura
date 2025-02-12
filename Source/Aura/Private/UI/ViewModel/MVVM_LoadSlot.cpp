3b42a3bf7988f241c9fde8e16e09676f46725489
Piotrr0 <piotrr.nawalany@gmail.com>
2025-02-12 14:47:32 +0100

    Set up switching slots

--- Source/Aura/Private/UI/ViewModel/MVVM_LoadSlot.cpp
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadSlot.cpp
@@ -1,2 +1,6 @@
 #include "UI/ViewModel/MVVM_LoadSlot.h"
 
+void UMVVM_LoadSlot::InitializeSlot()
+{
+	SetWidgetSwitcherIndex.Broadcast(1);
+}
--- Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
@@ -4,6 +4,8 @@
 #include "MVVMViewModelBase.h"
 #include "MVVM_LoadSlot.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSetWidgetSwitcherIndex, int32, WidgetSwitcherIndex);
+
 /**
  * 
  */
@@ -11,5 +13,21 @@ UCLASS()
 class AURA_API UMVVM_LoadSlot : public UMVVMViewModelBase
 {
 	GENERATED_BODY()
+
+public:
 	
+	UPROPERTY(BlueprintAssignable)
+	FSetWidgetSwitcherIndex SetWidgetSwitcherIndex;
+
+	void InitializeSlot();
+
+	UPROPERTY()
+	FString PlayerName;
+
+	UPROPERTY()
+	FString LoadSlotName;
+
+	UPROPERTY()
+	FString SlotIndex;
+
 };
